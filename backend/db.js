09febfe6ea3d1fed53469ee8d055082acbd8e5ee
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-06-19 05:09:04 +0000

    fix: connection URL error fixed

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // MongoDB connection string
-const mongoDB_URI = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.aaua1c8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const username = encodeURIComponent(process.env.MONGODB_USERNAME);
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+const mongoDB_URI = `mongodb+srv://${username}:${password}@cluster0.aaua1c8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const connectToMongo = async () => {
-    await mongoose.connect(mongoDB_URI, {
-        serverSelectionTimeoutMS: 15000,  // Increase timeout to handle slow networks
-        socketTimeoutMS: 20000,           // Optional: how long Mongoose waits for a response
-        ssl: true,                        // Ensure secure connection (especially for Atlas)
-    }).then(() => {
-        console.log("Connection is successfull :)");
-    }).catch((e) => {
-        console.log("NOT Connected :( " + e);
+  await mongoose
+    .connect(mongoDB_URI, {})
+    .then(() => {
+      console.log("Connection is successfull :)");
+    })
+    .catch((e) => {
+      console.log("NOT Connected :( " + e);
     });
-} 
+};
 
 module.exports = connectToMongo;
