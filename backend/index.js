57988e886f9ac9d7c50737ee73a71bf50ad27fba
mubassim08 <mubassimahmedkhan@gmail.com>
2025-06-25 06:37:59 +0000

    test: add multiple origins

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,15 @@ const app = express();
 const port = 8080 || process.env.REACT_APP_HOST_URL;
 
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://jubilant-carnival-r4xw7xg7x6q525x65-5173.app.github.dev',
+    'https://jubilant-carnival-r4xw7xg7x6q525x65-5173.app.github.dev/login',
+    'https://z-notebook-three.vercel.app'
+  ],
+  credentials: true,
+}));
 
 app.get('/', (req, res) => res.json({ 'message': 'Hello world' }).status(200))
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
